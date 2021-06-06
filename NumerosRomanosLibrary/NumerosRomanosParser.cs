a192ac4d3b84df99c12a7620dc5db9bbd512f92d
Rafael Silveira <rlsoft.rafael@outlook.com>
2021-06-05 22:51:05 -0300

    criacao do primeiro bloco do dojo, nao refatorado: 1 ao 9

---
+++ NumerosRomanosLibrary/NumerosRomanosParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NumerosRomanosLibrary
+{
+    public class NumerosRomanosParser
+    {
+        public int Parse(string numeroRomano)
+        {
+            int result = 0;
+
+            var regexParser = new Regex("^(M|MM|MMM)?(C|CC|CCC|CD|D|DC|DCC|DCCC|CM)?(X|XX|XXX|XL|L|LX|LXX|LXXX|XC)?(I|II|III|IV|V|VI|VII|VIII|IX)?$");
+            var parsed = regexParser.Match(numeroRomano);
+
+            var g1 = parsed.Groups[1].Value;
+            var g2 = parsed.Groups[2].Value;
+            var g3 = parsed.Groups[3].Value;
+            var g4 = parsed.Groups[4].Value;
+
+            switch (g4)
+            {
+                case "I":
+                    result += 1;
+                    break;
+                case "II":
+                    result += 2;
+                    break;
+                case "III":
+                    result += 3;
+                    break;
+                case "IV":
+                    result += 4;
+                    break;
+                case "V":
+                    result += 5;
+                    break;
+                case "VI":
+                    result += 6;
+                    break;
+                case "VII":
+                    result += 7;
+                    break;
+                case "VIII":
+                    result += 8;
+                    break;
+                case "IX":
+                    result += 9;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ NumerosRomanosUnitTestsLibrary/NumerosRomanosParserUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NumerosRomanosLibrary;
+using Xunit;
+
+namespace NumerosRomanosUnitTestsLibrary
+{
+    public class NumerosRomanosParserUnitTests
+    {
+        [Fact]
+        public void Parse_StringValidaI_Numero1Obtido()
+        {
+            var subject = new NumerosRomanosParser();
+
+            var result = subject.Parse("I");
+
+            result.Should().Be(1);
+        }
+
+        [Fact]
+        public void Parse_StringValidaII_Numero2Obtido()
+        {
+            var subject = new NumerosRomanosParser();
+
+            var result = subject.Parse("II");
+
+            result.Should().Be(2);
+        }
+
+        [Fact]
+        public void Parse_StringValidaV_Numero5Obtido()
+        {
+            var subject = new NumerosRomanosParser();
+
+            var result = subject.Parse("V");
+
+            result.Should().Be(5);
+        }
+
+    }
+}
